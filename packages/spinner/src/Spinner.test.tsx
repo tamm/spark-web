103e0ba1575acad530a6a425acfeeb3aad7a234a
Luke Bennett <hello@lukebennett.com.au>
2022-05-30 10:39:29 +1000

    Update imports

--- packages/spinner/src/Spinner.test.tsx
+++ packages/spinner/src/Spinner.test.tsx
@@ -2,8 +2,8 @@ import '@testing-library/jest-dom';
 
 import { render } from '@testing-library/react';
 
-import type { SpinnerProps } from '.';
-import { Spinner } from '.';
+import type { SpinnerProps } from './Spinner';
+import { Spinner } from './Spinner';
 
 jest.mock('@spark-web/utils', () => {
   const original = jest.requireActual('@spark-web/utils');
