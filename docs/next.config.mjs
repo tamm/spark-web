5348d54403b657997efd05ac1482f32e71e71ee4
Luke Bennett <hello@lukebennett.com.au>
2022-08-12 12:42:37 +1000

    Fix next config (#183)

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,8 +1,5 @@
 import withPreconstruct from '@preconstruct/next';
-import withPlugins from 'next-compose-plugins';
 import { withContentlayer } from 'next-contentlayer';
-
-import { withSearchIndex } from './utils/with-search-index.mjs';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -15,16 +12,4 @@ const nextConfig = {
   }),
 };
 
-export default withPlugins(
-  [
-    // Order is important here
-    // We need to run our plugin _after_ `withContentlayer` because we depend
-    // on contentlayer's output.
-    // Because of the order of operations, it has to come earlier in the array
-    // to be run later!
-    withSearchIndex,
-    withContentlayer,
-    withPreconstruct,
-  ],
-  nextConfig
-);
+export default withPreconstruct(withContentlayer(nextConfig));
